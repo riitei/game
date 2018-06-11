3f7b4d5c723d5eac5d97e7754d9d4f80266748b6
riitei <2590119197@qq.com>
2018-06-11 17:52:52 +0800

    DB讀取﻿map_coordinate 資料表

---
+++ php/map.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: riitei
+ * Date: 2018/6/11
+ * Time: 15:02
+ */
+
+
+include "DBConnection.php";
+
+// 取得 vertical 最大值
+class map
+{
+
+    public function mapJSON()
+    {
+        // 從DB的 vertical 資料欄位取得最大值
+        $vertical_sql = 'SELECT max(map_vertical) as vertical FROM game.map_coordinate;';
+        $vertical_sql_search = DBConnection::PDO()->query($vertical_sql)->fetch();
+        $max_vertical_value = $vertical_sql_search['vertical'];
+        // 從DB的 horizontal 資料欄位取得最大值
+        $horizontal_sql = 'SELECT max(map_horizontal) as horizontal FROM game.map_coordinate;';
+        $horizontal_sql_search = DBConnection::PDO()->query($horizontal_sql)->fetch();
+        $max_horizontal_value = $horizontal_sql_search['horizontal'];
+//
+        $map = array();// 存放DB讀取座標值
+//
+        for ($i = 0; $i <= $max_vertical_value; $i++) { //  DB 中 map_vertical值 從 0 到 map_vertical 最大值
+            for ($j = 0; $j <= $max_horizontal_value; $j++) {//  DB 中 map_vertical值 從 0 到 map_horizontal 最大值
+                // 得知 map_vertical 和 map_horizontal 參數，產生 sql 語句
+                $sql = "SELECT map_value FROM game.map_coordinate where map_vertical = '" . $i . "' and map_horizontal='" . $j . "';";
+                $map_value = DBConnection::PDO()->query($sql)->fetch();
+                // 從 sql 語句得到 ﻿map_value 值
+                // $map[vertical][horizontal] = $map_value['map_value'];
+                $map[$i][$j] = $map_value['map_value'];
+            }
+        }
+        return json_encode($map, true);
+
+    }
+}
+
+
+
+
+
