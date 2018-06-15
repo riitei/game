3575a153d52e121e01d05a59cae99b098b9c2717
riitei <2590119197@qq.com>
2018-06-15 15:27:56 +0800

    php api

--- php/map.php
+++ php/map.php
@@ -24,19 +24,23 @@ class map
         $horizontal_sql_search = DBConnection::PDO()->query($horizontal_sql)->fetch();
         $max_horizontal_value = $horizontal_sql_search['horizontal'];
 //
-        $map = array();// 存放DB讀取座標值
+        $map_value = array();// 存放DB讀取座標值
+        $photo_path = array();
 //
         for ($i = 0; $i <= $max_vertical_value; $i++) { //  DB 中 map_vertical值 從 0 到 map_vertical 最大值
             for ($j = 0; $j <= $max_horizontal_value; $j++) {//  DB 中 map_vertical值 從 0 到 map_horizontal 最大值
                 // 得知 map_vertical 和 map_horizontal 參數，產生 sql 語句
-                $sql = "SELECT map_value FROM game.map_coordinate where map_vertical = '" . $i . "' and map_horizontal='" . $j . "';";
-                $map_value = DBConnection::PDO()->query($sql)->fetch();
+//                $sql = "SELECT map_value FROM game.map_coordinate where map_vertical = '" . $i . "' and map_horizontal='" . $j . "';";
+                $sql2 = "SELECT map_coordinate.map_value,map_photo_path.map_photo_path FROM map_photo_path inner join map_coordinate on  map_photo_path.map_value = map_coordinate.map_value where map_vertical = '" . $i . "' and map_horizontal='" . $j . "'; ;";;
+                $map = DBConnection::PDO()->query($sql2)->fetch();
                 // 從 sql 語句得到 ﻿map_value 值
                 // $map[vertical][horizontal] = $map_value['map_value'];
-                $map[$i][$j] = $map_value['map_value'];
+                $map_value[$i][$j] = $map['map_value'];
+                $photo_path[$i][$j][$map['map_value']]=$map['map_photo_path'];
             }
         }
-        return json_encode($map, true);
+        return ['map'=>json_encode($map_value,true),'photo'=>json_encode($photo_path,true)];
+           // json_encode($map, true);
 
     }
 }
