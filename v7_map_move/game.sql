f015edea46466fc786a2ae9c1aecf98be31c32d6
riitei <2590119197@qq.com>
2018-07-17 11:23:12 +0800

    使用v5修改成map_move

---
+++ v7_map_move/game.sql
@@ -0,0 +1,104 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 192.168.0.104    Database: game
+-- ------------------------------------------------------
+-- Server version	5.6.38
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `map`
+--
+
+DROP TABLE IF EXISTS `map`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `map` (
+  `id` int(11) NOT NULL,
+  `checkpoint` varchar(45) NOT NULL COMMENT '關卡',
+  `map_array` longtext NOT NULL COMMENT '地圖map的array',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='將地圖array直接存放在db';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `map`
+--
+
+LOCK TABLES `map` WRITE;
+/*!40000 ALTER TABLE `map` DISABLE KEYS */;
+INSERT INTO `map` VALUES (1,'test','[[0, 0, 0, 0, 0], [0, 1, 0, 2, 0], [0, 1, 0, 1, 0], [0, 1, 0, 1, 0], [0, 0, 0, 1, 0], [0, 2, 0, 0, 0]]');
+/*!40000 ALTER TABLE `map` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `map_coordinate`
+--
+
+DROP TABLE IF EXISTS `map_coordinate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `map_coordinate` (
+  `id` int(11) NOT NULL,
+  `map_vertical` int(11) NOT NULL COMMENT '存放 map array 所有資訊',
+  `map_horizontal` int(11) NOT NULL,
+  `map_value` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='存放地圖座標';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `map_coordinate`
+--
+
+LOCK TABLES `map_coordinate` WRITE;
+/*!40000 ALTER TABLE `map_coordinate` DISABLE KEYS */;
+INSERT INTO `map_coordinate` VALUES (1,0,0,0),(2,0,1,0),(3,0,2,0),(4,0,3,0),(5,0,4,0),(6,1,0,0),(7,1,1,1),(8,1,2,0),(9,1,3,2),(10,1,4,0),(11,2,0,0),(12,2,1,1),(13,2,2,0),(14,2,3,1),(15,2,4,0),(16,3,0,0),(17,3,1,1),(18,3,2,0),(19,3,3,1),(20,3,4,0),(21,4,0,0),(22,4,1,0),(23,4,2,0),(24,4,3,1),(25,4,4,0),(26,5,0,0),(27,5,1,2),(28,5,2,0),(29,5,3,0),(30,5,4,0);
+/*!40000 ALTER TABLE `map_coordinate` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `map_photo_path`
+--
+
+DROP TABLE IF EXISTS `map_photo_path`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `map_photo_path` (
+  `id` int(11) NOT NULL,
+  `map_photo_name` varchar(45) NOT NULL COMMENT '存放地圖的名稱( pass 可通行 ,obstacle 不可通行,Character 角色)',
+  `map_photo_path` varchar(45) NOT NULL COMMENT '存放地圖的名稱( pass 可通行 ,obstacle 不可通行,Character 角色)的路徑',
+  `map_value` int(11) NOT NULL COMMENT '\n表格 map_coordinate \n資料表欄位 map_value 的 FK 值',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='存放地圖的圖片路徑';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `map_photo_path`
+--
+
+LOCK TABLES `map_photo_path` WRITE;
+/*!40000 ALTER TABLE `map_photo_path` DISABLE KEYS */;
+INSERT INTO `map_photo_path` VALUES (1,'pass','/photo/pass.jpg',0),(2,'obstacle','/photo/obstacle.jpg',1),(3,'character','/photo/character.jpg',2);
+/*!40000 ALTER TABLE `map_photo_path` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-15 12:58:02
---
+++ v7_map_move/index.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+
+    <title>讀取DB座標資訊</title>
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+
+    <style type="text/css">
+        /*圖片*/
+        .photo {
+            position: absolute;
+            margin-top: 100px;
+            margin-left: 100px;
+            overflow: hidden;
+            width: 100px;
+            height: 100px;
+        }
+
+        /*地圖範圍*/
+        .map {
+            position: absolute;
+            padding-top: 100px;
+            padding-left: 100px;
+            overflow: hidden;
+            z-index: 0;
+        }
+    </style>
+    <?php
+    include "php/map.php";
+    $map = new map();
+    $my_map = $map->mapInfo();
+    $my_map_array_value = $my_map['map'];// 讀取 map array 值
+    $my_map_photo_path = array();
+    $my_map_photo_path = $my_map['photo'];// 讀取 map array 地圖路徑
+    $my_map_vertical_Length = count($my_map_photo_path);// 欄
+    $my_map_horizontal_Length = count($my_map_photo_path[count($my_map_photo_path) - 1]);// 列
+    ?>
+
+    <script type="text/javascript">
+        // var map = [
+        //     [0, 0, 0, 0, 0],
+        //     [0, 1, 0, 2, 0],
+        //     [0, 1, 0, 1, 0],
+        //     [0, 1, 0, 1, 0],
+        //     [0, 0, 0, 1, 0],
+        //     [0, 2, 0, 0, 0]];
+        var map = <?=$my_map_array_value?>;
+        var verticalLength = <?=$my_map_vertical_Length?>;// 欄
+        var horizontalLength = <?=$my_map_horizontal_Length?>;// 列
+        var vertical = 5;// 圖片初始位置 陣列 欄
+        var horizontal = 0;// 圖片初始位置 陣列 列
+        var stop = false;// 控制是否能行走，預設可以行走
+        var map_vertical = 0;//控制地圖垂直方向
+        var map_horizontal = 0;//控制地圖水平方向
+        //
+        $(function () {
+            // 四張圖片重疊，初始座標位置
+            $("#start").css("top", vertical * 100);// 圖片初始位置 陣列 欄
+            $("#start").css("left", horizontal * 100);// 圖片初始位置 陣列 列
+            $("#top").css("top", vertical * 100);// 圖片初始位置 陣列 欄
+            $("#top").css("left", horizontal * 100);// 圖片初始位置 陣列 列
+            $("#down").css("top", vertical * 100);// 圖片初始位置 陣列 欄
+            $("#down").css("left", horizontal * 100);// 圖片初始位置 陣列 列
+            $("#left").css("top", vertical * 100);// 圖片初始位置 陣列 欄
+            $("#left").css("left", horizontal * 100);// 圖片初始位置 陣列 列
+            $("#right").css("top", vertical * 100);// 圖片初始位置 陣列 欄
+            $("#right").css("left", horizontal * 100);// 圖片初始位置 陣列 列
+            $("#map").css("height", verticalLength * 100);// 透過陣列 橫 得知地圖最大高度範圍
+            $("#map").css("width", horizontalLength * 100);//透過陣列 列 得知地圖最大長度範圍
+        });
+        $(document).keydown(function (event) {
+            $("#start").css("display", "none");
+            //
+            var move_place;
+            switch (event.which) {
+                case 37:// 鍵盤 左按鍵
+                    // 顯上左邊圖片
+                    photo_show("左", stop);
+                    // 移動
+                    move_place = move("左", vertical, horizontal);
+
+                    break;
+                case 38:// 鍵盤 上按鍵
+                    // 顯上上面圖片
+                    photo_show("上", stop);
+                    // 移動
+                    move_place = move("上", vertical, horizontal);
+                    break;
+                case 39:// 鍵盤 右按鍵
+                    // 顯上右邊圖片
+                    photo_show("右", stop);
+                    // 移動
+                    move_place = move("右", vertical, horizontal);
+                    break;
+                case 40:// 鍵盤 下按鍵
+                    // 顯上下面圖片
+                    photo_show("下", stop);
+                    // 移動
+                    move_place = move("下", vertical, horizontal);
+                    break;
+                default:
+                    break;
+            }// switch
+
+            if (move_place) {
+                var direction = pass(
+                    map[move_place["new_vertical"]][move_place["new_horizontal"]],
+                    move_place["new_vertical"],
+                    move_place["new_horizontal"],
+                    move_place["place_direction"],
+                    move_place["old_vertical"],
+                    move_place["old_horizontal"]);
+                vertical = direction.now_vertical;
+                horizontal = direction.now_horizonta;
+            }
+
+        });// keydown
+
+        // 圖片移動是否超出界線
+        function move(direction, vertical, horizontal) {
+            var coordinate = new Array();
+            // 紀錄原座標
+            coordinate["old_vertical"] = vertical;
+            coordinate["old_horizontal"] = horizontal;
+            // 初始新座標
+            coordinate["new_vertical"] = vertical;
+            coordinate["new_horizontal"] = horizontal;
+
+            switch (direction) {
+                case "左":
+                    if (horizontal > 0) { // 防止超過界線
+                        coordinate["new_horizontal"] = horizontal - 1; // 陣列向左移動
+                        coordinate["place_direction"] = "left"
+                    } else {
+                        coordinate = null;
+                    }
+                    break;
+                case "上":
+                    if (vertical > 0) { // 防止超過界線
+                        coordinate["new_vertical"] = vertical - 1;// 陣列向上移動
+                        coordinate["place_direction"] = "top";
+                    } else {
+                        coordinate = null;
+                    }
+                    break;
+                case"右":
+
+                    if (horizontal < horizontalLength - 1) { // 防止超過界線
+                        coordinate["new_horizontal"] = horizontal + 1;// 陣列向右移動
+                        coordinate["place_direction"] = "left";
+                    } else {
+                        coordinate = null;
+                    }
+                    break;
+                case"下":
+                    if (vertical < verticalLength - 1) { // 防止超過界線
+                        coordinate ["new_vertical"] = vertical + 1;// 陣列向下移動
+                        coordinate ["place_direction"] = "top";
+                    } else {
+                        coordinate = null;
+                    }
+                    break;
+            }//switch
+            return coordinate;
+
+        } // move
+
+        // 圖片顯示
+        function photo_show(direction, stop) {
+            if (stop == false) {
+                switch (direction) {
+                    case"左":
+                        $("#left").css("display", "inline");
+                        $("#top,#down,#right").css("display", "none");
+                        break;
+                    case "上":
+                        $("#top").css("display", "inline");
+                        $("#down,#left,#right").css("display", "none");
+                        break;
+                    case "右":
+                        $("#right").css("display", "inline");
+                        $("#top,#down,#left").css("display", "none");
+                        break;
+                    case "下":
+                        $("#down").css("display", "inline");
+                        $("#top,#left,#right").css("display", "none");
+                        break;
+                }
+            }
+
+        }
+
+        // 判斷是否通行
+        function pass(map, new_vertical, new_horizontal, place_direction, old_vertical, old_horizontal) {
+            var now_vertical;
+            var now_horizonta;
+            if (map == 0 && stop == false) {// 可以通行
+                switch (place_direction) {
+                    case "top":
+                        // 四個圖片同時移動
+                        $("#top").css("top", new_vertical * 100 + "px");
+                        $("#down").css("top", new_vertical * 100 + "px");
+                        $("#left").css("top", new_vertical * 100 + "px");
+                        $("#right").css("top", new_vertical * 100 + "px");
+                        break;
+                    case "left":
+                        $("#top").css("left", new_horizontal * 100 + "px");
+                        $("#down").css("left", new_horizontal * 100 + "px");
+                        $("#left").css("left", new_horizontal * 100 + "px");
+                        $("#right").css("left", new_horizontal * 100 + "px");
+                        break;
+                }
+                // 更新目前座標
+                now_vertical = new_vertical;
+                now_horizonta = new_horizontal;
+
+            } else if (map == 2) { // 角色
+                stop = true;// 鎖住鍵盤移動
+                photo_show("", stop);// 鎖定圖片顯示
+                $("#dialog").empty();
+                dialog(new_vertical, new_horizontal);
+                $("#dialog").css("display", "inline");
+                // 存放未移動座標位置
+                now_vertical = old_vertical;
+                now_horizonta = old_horizontal;
+            }
+            else { // 不可通行
+                // 存放未移動座標位置
+                now_vertical = old_vertical;
+                now_horizonta = old_horizontal;
+            }
+
+            return {"now_vertical": now_vertical, "now_horizonta": now_horizonta};
+
+        }
+
+        // 對話框
+        function dialog(vertical, horizontal) {
+            if (vertical == 5 && horizontal == 1) {// 對話框座標
+                // 添加對話框內容
+                $("#dialog").append(
+                    "yan" +
+                    "    <img src='photo/obstacle.jpg' class='dialog_photo'>" +
+                    "<br><br>" +
+                    "    <button id='t_close_01' type='button'>關閉</button>");
+            }
+            if (vertical == 1 && horizontal == 3) {// 對話框座標
+                // 添加對話框內容
+                $("#dialog").append(
+                    "ting" +
+                    "    <img src='photo/obstacle.jpg' class='dialog_photo'>" +
+                    "<br><br>" +
+                    "    <button id='t_close_01' type='button'>關閉</button>");
+            }
+        }
+
+    </script>
+
+</head>
+<body>
+<!--地圖-->
+<div id="map" class="map">
+    <!--繪製地圖的圖片-->
+    <?php
+    for ($vertical = 0; $vertical < $my_map_vertical_Length; $vertical++) {
+        for ($horizontal = 0; $horizontal < $my_map_horizontal_Length; $horizontal++) {
+            echo "<img src='" . $my_map_photo_path[$vertical][$horizontal] . "'class = 'map_photo'>";
+        }
+    }
+    ?>
+    <img id="start" class="photo" src="photo/start.jpg">
+    <img id="top" class="photo" src="photo/top.png" style="display: none">
+    <img id="down" class="photo" src="photo/down.png" style="display: none">
+    <img id="left" class="photo" src="photo/left.png" style="display: none">
+    <img id="right" class="photo" src="photo/right.png" style="display: none">
+    <!--區塊-->
+    <div id="block"></div>
+</div>
+
+<style type="text/css">
+    /*提示框*/
+    .dialog {
+        margin-left: 100px;
+        margin-top: 720px;
+        position: absolute;
+        width: 250px;
+        height: 100px;
+        background-color: #7b76a8;
+        display: none;
+        z-index: 1;
+    }
+
+    .dialog_photo {
+        position: absolute;
+        float: left;
+        width: 50px;
+        height: 50px;
+    }
+
+</style>
+
+<script>
+    $(function () {
+        $(document).on('click', '#t_close_01', function () {
+            stop = false;
+            $("#dialog").css("display", "none");
+        });
+    });
+</script>
+
+<div id="dialog" class="dialog"></div>
+</body>
+</html>
